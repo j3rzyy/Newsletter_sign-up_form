4647960b9e69886470fed05a636235158d4e321b
jerzy <egornik62@gmail.com>
2024-04-05 00:32:12 +0300

    make validate in form

--- src/components/CustomForm.tsx
+++ src/components/CustomForm.tsx
@@ -7,12 +7,19 @@ interface MyForm {
 }
 
 export const CustomForm: React.FC = () => {
-  const { register, handleSubmit } = useForm<MyForm>({
-    defaultValues: {},
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm<MyForm>({
+    mode: "onBlur",
   });
 
   const submit: SubmitHandler<MyForm> = (data) => {
-    console.log(data);
+    // console.log(data);
+    alert(JSON.stringify(data));
+    reset();
   };
 
   const error: SubmitErrorHandler<MyForm> = (data) => {
@@ -23,7 +30,18 @@ export const CustomForm: React.FC = () => {
     <div className="wrap">
       <form onSubmit={handleSubmit(submit, error)} className="form">
         <label className="inputHead">Email address</label>
-        <input {...register("email", { required: true })} value={'   email@company.com'} className="input"/>
+        <input
+          {...register("email", {
+            required: "Valid email required",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "invalid email address",
+            },
+          })}
+          // value="email@company.com"
+          className="input"
+        />
+        <div>{errors?.email && <p>{errors.email.message || "Error!"}</p>}</div>
         <button className="subButton">Subscribe on monthly newsletter</button>
       </form>
     </div>
